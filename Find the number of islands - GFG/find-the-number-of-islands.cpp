29dd7b8418a387c7b78d77b376dfe4e0e43aa3aa
eshanDhok17 <95692413+eshanDhok17@users.noreply.github.com>
2022-12-14 16:37:25 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -3,38 +3,42 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution {
-  public:
-    // Function to find the number of islands.
-    
-    void dfs(int row, int col, vector<vector<bool>> &vis, vector<vector<char>>& grid) {
+  private:
+    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &grid){
+        vis[row][col] = 1;
         int n = grid.size(), m = grid[0].size();
-        vis[row][col] = true;
-        for(int i=-1; i<=1; i++) {
+        for(int i=-1; i<=1; i++){
             for(int j=-1; j<=1; j++) {
-                int nr = row+i, nc = col+j;
-                if(nr>=0 and nr<n and nc>=0 and nc<m and grid[nr][nc] == '1' and !vis[nr][nc]) {
-                    dfs(nr, nc, vis, grid);
+                int nrow = row + i, ncol = col + j;
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1'){
+                    dfs(nrow, ncol, vis, grid);
                 }
             }
         }
         return ;
     }
     
+  public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size(), cnt = 0;
-        vector<vector<bool>> vis(n, vector<bool> (m, 0));
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(grid[i][j] == '1' and vis[i][j] == false) {
+        vector<vector<int>> vis(n, vector<int> (m,0));
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(vis[i][j] == 0 && grid[i][j] == '1'){
                     dfs(i, j, vis, grid);
                     cnt++;
                 }
             }
         }
+        
         return cnt;
     }
 };
+
+
 
 //{ Driver Code Starts.
 int main() {
