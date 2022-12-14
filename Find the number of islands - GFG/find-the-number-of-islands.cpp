6ec1aff413f6e31d7780fe5f504c2175263f8295
eshanDhok17 <95692413+eshanDhok17@users.noreply.github.com>
2022-12-14 16:40:24 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,22 +6,17 @@ using namespace std;
 
 class Solution {
   private:
-    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &grid){
-        vis[row][col] = 1;
+    void dfs(int &row, int &col, vector<vector<char>> &grid){
+        grid[row][col] = '0';
         int n = grid.size();
         int m = grid[0].size();
-        
-        int delRow[] = {-1,-1,0,1,1,1,0,-1};
-        int delCol[] = {0,1,1,1,0,-1,-1,-1};
-        
-        for(int i=0; i<8; i++){
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
-            
-            if(nrow >= 0 && nrow < n &&
-            ncol >= 0 && ncol < m &&
-            vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1'){
-                dfs(nrow, ncol, vis, grid);
+        for(int i=-1; i<=1; i++){
+            for(int j=-1; j<=1; j++) {
+                int nrow = row + i, ncol = col + j;
+                if(nrow >= 0 && nrow < n &&
+                ncol >= 0 && ncol < m && grid[nrow][ncol] == '1'){
+                    dfs(nrow, ncol, grid);
+                }
             }
         }
     }
@@ -29,12 +24,10 @@ class Solution {
   public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size(), cnt = 0;
-        vector<vector<int>> vis(n, vector<int> (m,0));
-        
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(vis[i][j] == 0 && grid[i][j] == '1'){
-                    dfs(i, j, vis, grid);
+                if(grid[i][j] == '1'){
+                    dfs(i, j, grid);
                     cnt++;
                 }
             }
