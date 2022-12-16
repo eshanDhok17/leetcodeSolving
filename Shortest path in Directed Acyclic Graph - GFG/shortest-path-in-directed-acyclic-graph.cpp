4ea42e28064440816ce1868d051b60eaa0a73e66
eshanDhok17 <95692413+eshanDhok17@users.noreply.github.com>
2022-12-16 19:01:24 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -7,45 +7,49 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 class Solution {
-private:
-    void topoSort(int node, vector<pair<int, int>> adj[], int vis[], stack<int> &st) {
+  public:
+    void topoSort(int node, bool vis[], vector<pair<int, int>> adj[], stack<int> &st) {
         vis[node] = true;
         for(auto &it : adj[node]) {
             int v = it.first;
             if(!vis[v]) {
-                topoSort(v, adj, vis, st);
+                topoSort(v, vis, adj, st);
             }
         }
         st.push(node);
         return ;
+        
     }
-  public:
-     vector<int> shortestPath(int n,int m, vector<vector<int>>& edges){
-        vector<pair<int, int>>adj[n];
+    vector<int> shortestPath(int n,int m, vector<vector<int>>& edges){
+        // code here
+        vector<pair<int, int>> adj[n];
         for(int i=0; i<m; i++) {
             int u = edges[i][0], v = edges[i][1], wt = edges[i][2];
             adj[u].push_back({v, wt});
         }
-        int vis[n] = {0};
         stack<int> st;
+        bool vis[n] = {false};
         for(int i=0; i<n; i++) {
             if(!vis[i]) {
-                topoSort(i, adj, vis, st);
+                topoSort(i, vis, adj, st);
             }
         }
+        
         vector<int> dist(n, 1e9);
         dist[0] = 0;
         while(!st.empty()) {
-            int node=st.top(); st.pop();
+            int node = st.top(); st.pop();
             for(auto &it : adj[node]) {
-                int v = it.first, wt = it.second;
-                if(dist[node] + wt < dist[v]) {
-                    dist[v] = dist[node] + wt;
+                int v = it.first;
+                if(dist[node] + it.second < dist[v]) {
+                    dist[v] = dist[node] + it.second;
                 }
             }
         }
         for(int i=0; i<n; i++) {
-            if(dist[i] == 1e9) dist[i] = -1;
+            if(dist[i] == 1e9) {
+                dist[i] = -1;
+            }
         }
         return dist;
     }
