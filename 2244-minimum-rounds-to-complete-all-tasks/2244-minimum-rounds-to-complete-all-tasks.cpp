a0d67c355f92c624acb72b2b365c14a978b3a6b0
Eshan Sanjay Dhok <95692413+eshanDhok17@users.noreply.github.com>
2023-01-04 12:35:57 +0530

    Time: 257 ms (73.24%), Space: 104.6 MB (34.09%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -6,32 +6,15 @@ public:
             mp[i]++;
         }
         int ans = 0;
-        while(mp.size() > 0) {
-            for(auto &it : mp) {
-                int a = it.first, b = it.second;
-                // cout << it.first << " " << it.second << endl;
-                if((b % 2 == 3 or b % 2 == 0 or b - 2 >= 0) and b % 3 != 0) {
-                    mp.erase(a);
-                    b = b-2;
-                    mp.insert({a, b});
-                    if(b == 0) {
-                        mp.erase(a);
-                        // cout << a << " udadiya" << endl;
-                    }
-                    ans += 1;
-                }
-                else if(b % 3 == 2 or b % 3 == 0) {
-                    mp.erase(a);
-                    b = b-3;
-                    mp.insert({a,b});
-                    if(b == 0) {
-                        mp.erase(a);
-                        // cout << a << " udadiya" << endl;
-                    }
-                    ans += 1;
-                }
-                else return -1;
-                // cout << ans << " iteration over" << endl;
+        for(auto &it : mp) {
+            int b = it.second;
+            if(b == 1) return -1;
+            if(b % 3 == 0) {
+                ans += b/3;
+            }
+            else {
+            // 
+                ans += (b/3) + 1;
             }
         }
         return ans;
