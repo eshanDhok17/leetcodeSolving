2aca17562966b0b8073167f077d3a96ebd316314
Eshan Sanjay Dhok <95692413+eshanDhok17@users.noreply.github.com>
2023-01-12 13:23:13 +0530

    Time: 177 ms (93.71%), Space: 60.5 MB (64.57%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,26 +1,27 @@
 class Solution {
-public:
-    unordered_set<int>vis;
-    int f(int node, vector<int> adj[], vector<bool> hasApple) {
-        int res = 0;
-        vis.insert(node);
-        for(int &i : adj[node]) {
-            if(!vis.count(i)) {
-                res += f(i, adj, hasApple);
+private:
+    int f(int &node, vector<bool> &vis, vector<vector<int>> &adj, vector<bool> &hasApple) {
+        vis[node] = true;
+        int apples = 0;
+        for(int &it : adj[node]) {
+            if(!vis[it]) {
+                apples += f(it, vis, adj, hasApple);
             }
         }
-        if(node == 0) return res;
-        if(hasApple[node] == true or res > 0) res += 2;
-        return res;
+        if(node == 0) return apples; // parent node
+        if(hasApple[node] or apples > 0) apples += 2;
+        return apples;
     }
+public:
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
         int ans = 0;
-        vector<int> adj[n];
+        vector<bool> vis(n, false);
+        vector<vector<int>> adj(n);
         for(int i=0; i<edges.size(); i++) {
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
-        
-        return f(0, adj, hasApple);
+        int node = 0;
+        return f(node, vis, adj, hasApple);
     }
 };
