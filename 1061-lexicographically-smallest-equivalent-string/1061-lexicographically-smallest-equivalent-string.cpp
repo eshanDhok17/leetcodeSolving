e1e8475570cf2d8e4ece59f2b8423e686cb686e2
Eshan Sanjay Dhok <95692413+eshanDhok17@users.noreply.github.com>
2023-01-15 19:14:27 +0530

    Time: 2 ms (84.40%), Space: 6.3 MB (94.50%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int par[26];
-    int find(int node) {
-        if(par[node] == node) return node;
-        return par[node] = find(par[node]);
+    void _union(int u, int v) {
+        u = _find(u), v = _find(v);
+        par[max(u, v)] = min(u, v);
     }
-    void uni(int v, int u) {
-        v = find(v), u = find(u);
-        if(v != u) par[max(v, u)] = min(v, u);
+    int _find(int node) {
+        if(node == par[node]) return node;
+        return par[node] = _find(par[node]);
     }
-    string smallestEquivalentString(string s1, string s2, string baseStr) {
+    string smallestEquivalentString(string s, string t, string b) {
         iota(par, par+26, 0);
-        int n=size(s1);
-        for(int i=0; i<n; i++) {
-            uni(s1[i]-'a', s2[i]-'a');
-        }
-        for(int i=0; i<baseStr.size(); i++) {
-            baseStr[i] = find(baseStr[i]-'a') + 'a';
-        }
-        return baseStr;
+        for(int i=0; i<size(s); i++)
+            _union(s[i] - 'a', t[i] - 'a');
+        
+        for(int i=0; i<size(b); i++)
+            b[i] = _find(b[i] - 'a') + 'a';
+        
+        return b;
     }
 };
