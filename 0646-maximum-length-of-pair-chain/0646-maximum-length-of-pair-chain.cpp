e89d1b7e5ef75d54879c82f3a406d2f3e1edd86d
Eshan Sanjay Dhok <95692413+eshanDhok17@users.noreply.github.com>
2023-01-21 01:36:42 +0530

    Time: 70 ms (84.60%), Space: 23 MB (33.25%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
+    /*
+        Intuition:
+            1. Merge all intervals and keep the merged intervals [i][1] to minimum
+               which will assure us that the previous interval is in consideration
+               and the merged one is discarded.
+               basically we have to discard intervals which can be merged as they cannot
+               satisfy the condition.
+            2. return the merge interval's size which will be [a,b], [c,d]
+               where b < c.
+    */
     int findLongestChain(vector<vector<int>>& pairs) {
         sort(pairs.begin(), pairs.end());
-        vector<vector<int>> output;
-        output.push_back(pairs[0]);
-        for(int i=1; i<pairs.size(); i++) {
-            if(output.back()[1] >= pairs[i][0]) {
-                output.back()[1] = min(output.back()[1] , pairs[i][1]);
+        vector<vector<int>> merged;
+        merged.push_back(pairs[0]);
+        for(int i=1, j=0; i<pairs.size(); i++) {
+            if(merged[j][1] >= pairs[i][0]) {
+                merged[j][1] = min(merged[j][1], pairs[i][1]);
             }
-            else output.push_back(pairs[i]); 
+            else {
+                merged.push_back(pairs[i]);
+                j++;
+            }
         }
-        return output.size();
+        return size(merged);
     }
 };
