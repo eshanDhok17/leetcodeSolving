7d1f11bd93fbf455508713cce71b4c0c3245ecfd
Eshan Sanjay Dhok <95692413+eshanDhok17@users.noreply.github.com>
2023-01-25 00:22:07 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (96.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int dp[101];
-    int solve(int i, vector<int> &nums) {
+    int f(int i, vector<int> &nums) {
         if(i < 0) return 0;
         if(dp[i] != -1) return dp[i];
-        return dp[i] = max(nums[i] + solve(i-2, nums), solve(i-1, nums));
+        return dp[i] = max(f(i-1, nums), f(i-2, nums) + nums[i]);
     }
     int rob(vector<int>& nums) {
         memset(dp, -1, sizeof(dp));
-        return solve(size(nums)-1, nums);
+        return f(nums.size()-1, nums);
     }
 };
